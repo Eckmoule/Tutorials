0def14c2edc99f797adbbbea66e2d93477636c56
Yohan Béjannin <yohan.bejannin@gmail.com>
2015-07-02 22:31:40 +0200

    First console play for rails

--- Rails/Level2/consolePlay.ps1
+++ Rails/Level2/consolePlay.ps1
@@ -1,2 +1,28 @@
+#Initialize application 
+
 > rails new readingList
 > rails s
+> rails generate scaffold Book title author descrition:text amazon_id
+> rake db:migrate
+
+#Fill the db/seed.rb file (used to create data)
+
+> rake db:seed 
+
+#Fill the config/routes.rb file to define the root route 
+
+#It's possible to run the console in sandbox box. Every modifications will be remove on exit (rails c --sandbox)
+
+#Creation of a database migration 
+
+> rails g migration add_rating_to_books rating:integer 
+> rake db:migrate
+
+#In rails the convention if _at for datetime and _on for date 
+
+> rails g migration add_finished_on_to_books finished_on:date
+> rake db:migrate 
+
+#Add rating and date on the seed file
+
+> rake db:seed
--- Rails/Level2/readingList/db/seeds.rb
+++ Rails/Level2/readingList/db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.destroy_all
+
+Book.create!([{
+	title: "Zero to One",
+	author: "Peter Thiel",
+	description: "Zero to One is a refreshing intellectual deep dive into the motives behind entrepreneurship. It’s full of unique, practical insights, and discusses success in terms of human nature and culture. Along with business strategy, Thiel outlines how successful innovation shapes society and shares an intriguing vision.",
+	amazon_id: "0804139296",
+	rating: 5,
+	finished_on: 10.days.ago,
+},
+{
+	title: "The Lean Startup",
+	author: "Eric Ries",
+	description: "Most startups fail. But many of those failures are preventable.  The Lean Startup is a new approach being adopted across the globe, changing the way companies are built and new products are launched.",
+	amazon_id: "0307887898",
+	rating: 4,
+	finished_on: 5.days.ago,
+},
+{
+	title: "Elon Musk",
+	author: "Ashley Vance",
+	description: "In Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future, veteran technology journalist Ashlee Vance provides the first inside look into the extraordinary life and times of Silicon Valley's most audacious entrepreneur. Written with exclusive access to Musk, his family and friends, the book traces the entrepreneur's journey from a rough upbringing in South Africa to the pinnacle of the global business world.",
+	amazon_id: "0062301233",
+	rating: 5,
+	finished_on: nil,
+}])
+
+p "Created #{Book.count} books"
